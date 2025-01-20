b0258171822b086ba1a37839f3138befd69087be
rionTamamushi <>
2025-01-20 11:14:20 +0900

    first commit

---
+++ src/main/java/com/example/demo/TamamushiKadai0120Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamamushiKadai0120Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamamushiKadai0120Application.class, args);
+	}
+
+}
